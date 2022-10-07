84745a529c6c51d2ceac6cf206adef988c0dd974
Ümmühan Kavas <ummuhankavas@gmail.com>
2022-10-07 12:42:46 +0200

    add btn onclick  added

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const delBtn = document.getElementById('delete');
 const addBtn = document.getElementById('add');
 
 const languageSection = document.querySelector('#language-section');
+
+
+const ul = document.createElement('ul');
+languageSection.appendChild(ul);
+
+addBtn.onclick = function() {
+    if (!languageInput.value){
+        alert('Please write a language');
+    } else{
+        ul.innerHTML=`<li>${languageInput.value}</li>`;
+        languageInput.value = '';
+    }
+   
+}
