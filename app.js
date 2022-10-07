d7433f5b09a95cf67f3a648951cc2573c260e486
Ümmühan Kavas <ummuhankavas@gmail.com>
2022-10-07 12:12:29 +0200

    elements done

---
+++ app.js
@@ -0,0 +1,5 @@
+const languageInput = document.querySelector('.language');
+const delBtn = document.getElementById('delete');
+const addBtn = document.getElementById('add');
+
+const languageSection = document.querySelector('#language-section');
